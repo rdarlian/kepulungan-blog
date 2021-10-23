b14e297bbd5548568c9ed568ddd7f1921436faa1
rdarlian <darlianramadhan@gmail.com>
2021-10-23 16:27:14 +0700

    add admin

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -21,9 +21,10 @@ class RegisterController extends Controller
             'name' => 'required|max:255',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
             'email' => 'required|email:dns|unique:users',
-            'password' => 'required|min:5'
+            'password' => 'required|min:5',
         ]);
 
+        $validatedData['is_admin'] = true;
         $validatedData['password'] = bcrypt($validatedData['password']);
 
         User::create($validatedData);
