0b4ae5ae3abdf466e52eb1cf062c2bbcb6d18685
rdarlian <darlianramadhan@gmail.com>
2021-10-23 15:46:50 +0700

    ganti navbar dan berita

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,7 +57,7 @@
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>Berita</h2>
+          <h2>Koran</h2>
           <h2>Seputar Desa <span>Kepulungan</span></h2>
 
         </div>
